dd0b3dba24759035b48c36ed2bb4fdd5f5af2c2e
Sydney Cheng <sydney_cheng1@baylor.edu>
2021-10-04 17:16:30 -0500

    list comprehension

--- json1.py
+++ json1.py
@@ -1,6 +1,6 @@
 import json
 
-infile = open('eq_data_1_day_m1.json','r')
+infile = open('eq_data_30_day_m1.json','r')
 outfile = open('readable_eq_data.json','w')
 
 #this converts contents of the json file into a dictionary
@@ -21,12 +21,23 @@ lons = []   #longitudes
 
 for eq in list_of_eqs:   #eq is a dictionary
     mag = eq["properties"]["mag"]
-    lat = eq["geometry"]["coordinates"][0]
-    lon = eq["geometry"]["coordinates"][1]
+    lat = eq["geometry"]["coordinates"][1]
+    lon = eq["geometry"]["coordinates"][0]
     mags.append(mag)
     lats.append(lat)
     lons.append(lon)
 
-print(mags)
-print(lats)
-print(lons)
+print(mags[:5])
+print(lats[:5])
+print(lons[:5])
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [Scattergeo(lon=lons, lat=lats)] #the orange (lon,lat) are arguments; lons and lats are lists
+
+my_layout = Layout(title="Global Earthquakes 1 Day")
+
+fig = {"data": data, "layout": my_layout}
+
+offline.plot(fig, filename="globalearthquakes1day.html")
